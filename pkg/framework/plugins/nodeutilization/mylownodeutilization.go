42ed3d30b028d6b8aea7e339396e904a043c5986
adie2 <12277615+adie2@user.noreply.gitee.com>
2024-05-08 15:37:45 +0800

    1

--- pkg/framework/plugins/nodeutilization/mylownodeutilization.go
+++ pkg/framework/plugins/nodeutilization/mylownodeutilization.go
@@ -56,7 +56,8 @@ func (m *MyLowNodeUtilization) Balance(ctx context.Context, nodes []*v1.Node) *f
 	klog.V(1).InfoS("自定义插件启动成功")
 
 	klog.V(1).InfoS("node capacity 信息")
-	fmt.Printf(nodes[0].Status.String())
+	klog.V(1).InfoS(nodes[0].Status.String())
+
 	klog.V(1).InfoS("node capacity 信息")
 
 	// check if Pods/CPU/Mem are set, if not, set them to 100
